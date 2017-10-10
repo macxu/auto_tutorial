87d39c0f1104b3227a2e0d3e54ebf2c57ad15b83
Mac Xu <mxu@marinsoftware.com>
2017-10-10 17:35:23 +0800

    add buggy code for tutorial

--- app/modules/string/stringProcessor.py
+++ app/modules/string/stringProcessor.py
@@ -4,6 +4,39 @@ class StringProcessor:
     def __init__(self):
         pass
 
+    # @staticmethod
+    # def reverse_and_merge(original_string):
+    #
+    #     result_string = original_string[-1]
+    #
+    #     index = len(original_string) - 2
+    #     while index >= 0:
+    #         if result_string[-1] != original_string[index]:
+    #             result_string += original_string[index]
+    #
+    #         index -= 1
+    #
+    #     return result_string
+
+
+    # @staticmethod
+    # def reverse_and_merge(original_string):
+    #
+    #     if not original_string or len(original_string) < 2:
+    #         return original_string
+    #
+    #     result_string = original_string[-1]
+    #
+    #     index = len(original_string) - 2
+    #     while index > 0:
+    #         if result_string[-1] != original_string[index]:
+    #             result_string += original_string[index]
+    #
+    #         index -= 1
+    #
+    #     return result_string
+
+
     @staticmethod
     def reverse_and_merge(original_string):
 
